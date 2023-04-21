a3bd34845f23138067eece48a3f1e339baee9d0a
TrevorJA <89143989+TrevorJA@users.noreply.github.com>
2023-04-21 16:58:14 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+
+## Data sources
+
+
+
+Geospatial Fabric database:
+https://www.sciencebase.gov/catalog/item/5e29b87fe4b0a79317cf7df5
+
+Point of Interest IDs (`poi_gage_segment.csv` and `poi_gage_id.csv`) were retrieved from: 
+https://github.com/nhm-params-v10-usgs/paramdb_v10_daymet_CONUS
+
---
+++ nhm_drb_site_matches.py
@@ -0,0 +1,34 @@
+## NHM segment IDs or USGS-IDs (USGS-IDs start with 0)
+nhm_site_matches = {'cannonsville': [1562],
+                    'pepacton': [1449],
+                    'neversink': [1645],
+                    'wallenpaupack': [1602], 
+                    'prompton': [1586],
+                    'shoholaMarsh': [1598], ## Note, Shohola has no inlet gauge or NHM segment - use wallenpaupack upstream
+                    'mongaupeCombined': [1640], 
+                    'beltzvilleCombined': [1710], 
+                    'fewalter': [1694],
+                    'merrillCreek': [1467], ## Merrill Creek doesnt have gage or HRU - using nearby small stream flow
+                    'hopatcong': [1470],  ## NOTE, this is downstream but should be good
+                    'nockamixon': [1470],
+                    'assunpink': [1496],  ## NOTE, this is downstream of reservoir but above the link, should be good
+                    'ontelaunee': [2279], 
+                    'stillCreek': [2277],  ## Note, this is downstream of reservoir and lakes
+                    'blueMarsh': [2335],
+                    'greenLane': [2310],
+                    'marshCreek': [2009],
+                    '01425000': [1566],
+                    '01417000': [1444],
+                    'delLordville': [1573],
+                    '01436000': [1638],
+                    '01433500': [1647],
+                    'delMontague': [1659],
+                    '01449800': [1697],
+                    '01447800': [1695],
+                    'delTrenton': [1498],
+                    '01463620': [1492],
+                    'outletAssunpink': [1493],
+                    '01470960': [2333],
+                    'outletSchuylkill': [2338],
+                    'outletChristina': [2005]
+                    }
